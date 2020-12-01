ab2e59f5bec6ab1250faea2be29b95c21dc79ff3
Egor Sychev <sychev.2017@nstu.ru>
2020-12-02 02:20:07 +0700

    add docstrings

--- pythonProject1/nstu_api.py
+++ pythonProject1/nstu_api.py
@@ -4,10 +4,23 @@ import json
 
 class NSTU_API:
     def __init__(self, url='https://api.ciu.nstu.ru/v1.0/tgbot/', api_key='s13dfget456DADHGWEv34g435f'):
+        """Конструктор.
+
+        url -- Адрес, по которому будет обращение к API НГТУ
+        api_key -- Ключ для аутентификации запросов
+        """
+
+        if not url.endswith('/'):
+            url += '/'
         self.url = url
         self.api_key = api_key
 
     def __getStudentID__(self, phone):
+        """Получить id и хэш студента по его номеру телефона. Возвращает список словарей с полями `ID` и `HASH`.
+
+        phone -- Телефонный номер студента, указанный в его личном кабинете
+        """
+
         headers = {
             'Content-Type': 'application/json',
             'Http-Api-Key': self.api_key,
@@ -23,6 +36,10 @@ class NSTU_API:
             raise requests.exceptions.HTTPError(response)
 
     def __getStudentInfo__(self, id, hash):
+        """Получить основную информацию о студенте по его `id` и `hash`.
+        Получить их можно методом `__getStudentID__`.
+        Возвращает список словарей с полями `STATUS`, `FIO`, `FORM`, `BASIS`"""
+
         headers = {
             'Content-Type': 'application/json',
             'Http-Api-Key': self.api_key
@@ -39,4 +56,14 @@ class NSTU_API:
             raise requests.exceptions.HTTPError(response)
 
     def getStudentInfo(self, phone):
-        return [self.__getStudentInfo__(id_hash['ID'], id_hash['HASH']) for id_hash in self.__getStudentID__(phone)]
+        """Получить основную информацию о студенте по его номеру телефона.
+        Возвращает список словарей с полями `STATUS`, `FIO`, `FORM`, `BASIS`.
+
+        phone -- Телефонный номер студента, указанный в его личном кабинете
+        """
+
+        result = []
+        for id_hash in self.__getStudentID__(phone):
+            for info in self.__getStudentInfo__(id_hash['ID'], id_hash['HASH']):
+                result.append(info)
+        return info
