2dd3d2960d97ef0928fd8fba5aa993e3c4a10ca4
Egor Sychev <sychev.2017@nstu.ru>
2020-12-07 21:59:53 +0700

    add logging

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -8,9 +8,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
+import logging
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 button_mail = 'Бот не помог'
 stats = {'intent': 0, 'failure': 0}
