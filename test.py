362b028d691f60f7aa4fd28678b78d4aeb89971b
Egor Sychev <sychev.2017@nstu.ru>
2020-12-09 04:28:56 +0700

    test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import json
 r = requests.post('http://localhost:5005/model/parse',
                   json={'text': 'студент 89658242328'},
                   headers={'content-type': 'application/json'})
-print(r.text)
+print(json.loads(r.text)['text'], ' -> ', json.loads(r.text))
 r = requests.post('http://localhost:5005/model/parse',
                   json={'text': 'преподаватель'},
                   headers={'content-type': 'application/json'})
-print(r.text)
+print(json.loads(r.text)['text'], ' -> ', json.loads(r.text))
